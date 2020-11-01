d3a1ed265b6baf3d321305d7fa7691482727e9ca
SophiePenny <512894@chrome.escg.ac.uk>
2020-11-01 16:01:44 +0000

    tried to get the rabbit to move

---
+++ Assets/Scripts/rabbitmove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class rabbitmove : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 Velocity = rb.velocity;
+        if (Input.GetKey("a"))
+        {
+            Velocity.x = -100;
+        }
+        if (Input.GetKey("d"))
+        {
+            Velocity.x = 100;
+        }
+        rb.velocity = Velocity;
+        Velocity.y = 0;
+    }
+}
